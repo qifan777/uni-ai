19c54500c9b134c8022185c55cb7565f08ea4562
linjiacheng <1507906763@qq.com>
2024-06-06 13:47:52 +0800

    fix: 打包剔除private

--- uni-ai-backend/spring-ai-dashscope-spring-boot-starter/src/main/java/io/qifan/ai/dashscope/DashScopeAiEmbeddingOptions.java
+++ uni-ai-backend/spring-ai-dashscope-spring-boot-starter/src/main/java/io/qifan/ai/dashscope/DashScopeAiEmbeddingOptions.java
@@ -1,5 +1,6 @@
 package io.qifan.ai.dashscope;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.springframework.ai.embedding.EmbeddingOptions;
@@ -7,5 +8,6 @@ import org.springframework.ai.embedding.EmbeddingOptions;
 @Data
 @Accessors(chain = true)
 public class DashScopeAiEmbeddingOptions implements EmbeddingOptions {
+    @JsonProperty("model")
     private String model;
 }
--- uni-ai-backend/spring-ai-qianfan-spring-boot-starter/src/main/java/io/qifan/ai/qianfan/QifanAiEmbeddingOptions.java
+++ uni-ai-backend/spring-ai-qianfan-spring-boot-starter/src/main/java/io/qifan/ai/qianfan/QifanAiEmbeddingOptions.java
@@ -1,5 +1,6 @@
 package io.qifan.ai.qianfan;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.springframework.ai.embedding.EmbeddingOptions;
@@ -7,5 +8,6 @@ import org.springframework.ai.embedding.EmbeddingOptions;
 @Data
 @Accessors(chain = true)
 public class QifanAiEmbeddingOptions implements EmbeddingOptions {
+    @JsonProperty("model")
     String model;
 }
