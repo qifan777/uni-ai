3a449348e360643cec1deaa417056794a9d49498
linjiacheng <1507906763@qq.com>
2024-06-14 10:11:28 +0800

    build: docker-compose

--- uni-ai-backend/scripts/build.sh
+++ uni-ai-backend/scripts/build.sh
@@ -1,8 +1,8 @@
-cd ../../uni-ai-admin
-npm run build-only
-cp -r ./dist ../uni-ai-backend/server/src/main/resources
-cd ../uni-ai-backend
-gradle server:bootJar
-cd scripts
-docker build -t qifan7/uni-ai:0.1.0-snapshot -f Dockerfile ../server
-docker push qifan7/uni-ai:0.1.0-snapshot
+#cd ../../uni-ai-admin
+#npm run build-only
+#cp -r ./dist ../uni-ai-backend/server/src/main/resources
+#cd ../uni-ai-backend
+#mvn -DskipTests=true package
+#cd scripts
+docker build -t qifan7/uni-ai:0.1.4 -f Dockerfile ../server
+docker push qifan7/uni-ai:0.1.4
